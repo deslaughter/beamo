41e49a8822a3343d662a2210bc0bc5d746600d5e
Derek Slaughter <deslaughter@gmail.com>
2025-01-23 14:15:20 -0500

    Add trapezoidal quadrature

--- src/quadrature.rs
+++ src/quadrature.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 #[derive(Clone)]
 pub struct Quadrature {
     pub points: Vec<f64>,
@@ -25,5 +27,28 @@ impl Quadrature {
             points: points,
             weights: weights,
         }
+    }
+    pub fn trapezoidal(s: &[f64]) -> Self {
+        let n = s.len();
+        if n < 2 {
+            panic!("insufficient points")
+        }
+
+        // Calculate points
+        let min = s.iter().cloned().fold(f64::INFINITY, f64::min);
+        let max = s.iter().cloned().fold(f64::NEG_INFINITY, f64::max);
+        let points = s
+            .iter()
+            .map(|&x| -1.0 + 2.0 * (x - min) / (max - min))
+            .collect_vec();
+
+        // Calculate weights
+        let mut weights = vec![0.; n];
+        weights[0] = (points[1] - points[0]) / 2.;
+        for i in 1..n - 1 {
+            weights[i] = (points[i + 1] - points[i - 1]) / 2.;
+        }
+        weights[n - 1] = (points[n - 1] - points[n - 2]) / 2.;
+        Self { points, weights }
     }
 }
