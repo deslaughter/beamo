8507b97ae49d53ee4dc80ec4fe5ade5eeeca9cef
Derek Slaughter <deslaughter@gmail.com>
2025-01-23 14:15:45 -0500

    Improve methods for reading BeamDyn files

--- src/external.rs
+++ src/external.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::f64::consts::PI;
 
 use faer::{col, Mat};
@@ -7,6 +8,53 @@ use crate::{
     elements::beams::BeamSection,
     util::{quat_as_matrix, Quat},
 };
+
+pub fn parse_beamdyn_keypoints(file_data: &str) -> Vec<[f64; 4]> {
+    let lines = file_data.lines().collect_vec();
+
+    let member_total_line = lines.get(19).unwrap();
+    if !member_total_line.contains("member_total") {
+        panic!("line 20 doesn't contain member_total")
+    }
+    let member_total: usize = member_total_line
+        .split_whitespace()
+        .collect_vec()
+        .first()
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    if member_total != 1 {
+        panic!("member_total must equal 1")
+    }
+
+    // Get the number of key points
+    let kp_total_line = lines.get(20).unwrap();
+    if !kp_total_line.contains("kp_total") {
+        panic!("line 21 doesn't contain kp_total")
+    }
+    let kp_total = kp_total_line
+        .split_whitespace()
+        .collect_vec()
+        .first()
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    // Get key point coordinates and twist (swap x and z so length is along x)
+    lines
+        .iter()
+        .skip(24)
+        .take(kp_total)
+        .map(|l| {
+            let v = l
+                .split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect_vec();
+            [v[2], v[1], v[0], v[3]]
+        })
+        .collect_vec()
+}
 
 pub fn parse_beamdyn_sections(file_data: &str) -> Vec<BeamSection> {
     let mut m = Mat::<f64>::zeros(3, 3);
@@ -18,17 +66,17 @@ pub fn parse_beamdyn_sections(file_data: &str) -> Vec<BeamSection> {
     let mut m_rot = Mat::<f64>::zeros(6, 6);
     m_rot.submatrix_mut(0, 0, 3, 3).copy_from(&m);
     m_rot.submatrix_mut(3, 3, 3, 3).copy_from(&m);
-    file_data
-        .lines()
-        .skip(10)
+    let lines = file_data.lines().skip(10).collect_vec();
+    lines
+        .iter()
         .chunks(15)
         .into_iter()
-        .map(|chunk| {
-            let lines = chunk.collect_vec();
-            let s = lines[0].parse::<f64>().unwrap();
+        .map(|chunks| {
+            let ls = chunks.collect_vec();
+            let s = ls[0].trim().parse::<f64>().unwrap();
             let c = (1..7)
                 .map(|i| {
-                    lines[i]
+                    ls[i]
                         .split_ascii_whitespace()
                         .filter_map(|s| s.parse::<f64>().ok())
                         .collect_vec()
@@ -36,7 +84,7 @@ pub fn parse_beamdyn_sections(file_data: &str) -> Vec<BeamSection> {
                 .collect_vec();
             let m = (8..14)
                 .map(|i| {
-                    lines[i]
+                    ls[i]
                         .split_ascii_whitespace()
                         .filter_map(|s| s.parse::<f64>().ok())
                         .collect_vec()
@@ -61,21 +109,57 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_parse_beamdyn_keypoints() {
+        let keypoints = parse_beamdyn_keypoints(BD_INPUT1);
+        println!("{:?}", keypoints);
+    }
+
+    #[test]
     fn test_parse_beamdyn_sections() {
-        let sections = parse_beamdyn_sections(BD_INPUT);
+        let sections = parse_beamdyn_sections(BD_INPUT2);
         println!("{:?}", sections);
     }
 
-    const BD_INPUT: &str =
+    const BD_INPUT1: &str = r#"--------- BEAMDYN with OpenFAST INPUT FILE -------------------------------------------
+bar_urc blade
+---------------------- SIMULATION CONTROL --------------------------------------
+True          Echo             - Echo input data to "<RootName>.ech"? (flag)
+False         QuasiStaticInit  - Use quasi-static pre-conditioning with centripetal accelerations in initialization? (flag) [dynamic solve only]
+          0   rhoinf           - Numerical damping parameter for generalized-alpha integrator
+          1   quadrature       - Quadrature method: 1=Gaussian; 2=Trapezoidal (switch)
+          1   refine           - Refinement factor for trapezoidal quadrature (-) [DEFAULT = 1; used only when quadrature=2]
+"DEFAULT"     n_fact           - Factorization frequency for the Jacobian in N-R iteration(-) [DEFAULT = 5]
+"DEFAULT"     DTBeam           - Time step size (s)
+50     load_retries     - Number of factored load retries before quitting the simulation [DEFAULT = 20]
+"DEFAULT"     NRMax            - Max number of iterations in Newton-Raphson algorithm (-) [DEFAULT = 10]
+"DEFAULT"     stop_tol         - Tolerance for stopping criterion (-) [DEFAULT = 1E-5]
+"DEFAULT"     tngt_stf_fd      - Use finite differenced tangent stiffness matrix? (flag)
+"DEFAULT"     tngt_stf_comp    - Compare analytical finite differenced tangent stiffness matrix? (flag)
+"DEFAULT"     tngt_stf_pert    - Perturbation size for finite differencing (-) [DEFAULT = 1E-6]
+"DEFAULT"     tngt_stf_difftol - Maximum allowable relative difference between analytical and fd tangent stiffness (-); [DEFAULT = 0.1]
+True          RotStates        - Orient states in the rotating frame during linearization? (flag) [used only when linearizing]
+---------------------- GEOMETRY PARAMETER --------------------------------------
+          1   member_total    - Total number of members (-)
+          5   kp_total        - Total number of key points (-) [must be at least 3]
+     1     5                  - Member number; Number of key points in this member
+		 kp_xr 			 kp_yr 			 kp_zr 		 initial_twist
+		  (m)  			  (m)  			  (m)  		   (deg)
+	 0.00000e+00 	 0.00000e+00 	 0.00000e+00 	 0.00000e+00
+	 0.00000e+00 	 0.00000e+00 	 2.04082e+00 	 0.00000e+00
+	 0.00000e+00 	 0.00000e+00 	 4.08163e+00 	 0.00000e+00
+	 0.00000e+00 	 0.00000e+00 	 6.12245e+00 	 0.00000e+00
+	 0.00000e+00 	 0.00000e+00 	 8.16327e+00 	 0.00000e+00 "#;
+
+    const BD_INPUT2: &str =
         "------- BEAMDYN V1.00.* INDIVIDUAL BLADE INPUT FILE --------------------------
-! NACA 0012 airfoil with chord 0.1 - Written using beamdyn.py 
+! NACA 0012 airfoil with chord 0.1 - Written using beamdyn.py
 ---------------------- BLADE PARAMETERS --------------------------------------
    21  station_total    - Number of blade input stations (-)
     1  damp_type        - Damping type (switch): 0: no damping; 1: viscous damping
 ---------------------- DAMPING COEFFICIENT------------------------------------
    mu1        mu2        mu3        mu4        mu5        mu6
    (s)        (s)        (s)        (s)        (s)        (s)
- 0.01 0.01 0.01 0.01 0.01 0.01 
+ 0.01 0.01 0.01 0.01 0.01 0.01
 ---------------------- DISTRIBUTED PROPERTIES---------------------------------
 0.000000
    6.804663E-18 0.000000E+00 0.000000E+00 0.000000E+00 0.000000E+00 -1.549113E-28
